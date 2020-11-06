df466e49470a11a6ec9f0c38bde456c4c4a5c92f
Joel <joelkalich1111@gmail.com>
2020-11-05 23:24:20 -0800

    Removed title bar and hid buttons

--- CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/MainActivity.java
+++ CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.cardgamesuiteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.Window;
 
 import com.example.cardgamesuiteapp.display.Card;
 
@@ -12,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Card card = ((Card)findViewById(R.id.cardtodisplay));
+        Card card = ((Card) findViewById(R.id.cardtodisplay));
         card.setCard(7);
     }
 }
