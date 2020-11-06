05fddc7fd8cae3993f1eb46f78a0daf0cc1aaf27
ekelley9 <57920465+ekelley9@users.noreply.github.com>
2020-11-05 17:34:02 -0800

    Stubbed out the card and hand methods for later use

--- CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/display/Card.java
+++ CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/display/Card.java
@@ -1,4 +1,31 @@
 package com.example.cardgamesuiteapp.display;
 
-public class Card {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Card extends View implements View.OnTouchListener {
+
+//    String that keeps track of the drawable to be displayed
+    private String card = "";
+
+    public Card(Context context) {
+        super(context);
+    }
+
+//  will determine which card needs to be displayed based on input by the standard class
+    public void determineCard(String cardToDisplay){
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
 }
--- CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/display/Hand.java
+++ CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/display/Hand.java
@@ -1,11 +1,20 @@
 package com.example.cardgamesuiteapp.display;
 
+import android.content.Context;
+import android.view.ViewGroup;
+
 import java.util.LinkedList;
 
-public class Hand {
+public class Hand extends ViewGroup {
     LinkedList<Card> cards;
 
-    public Hand(){
+    public Hand(Context context ){
+        super(context);
+
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
 
     }
 }
