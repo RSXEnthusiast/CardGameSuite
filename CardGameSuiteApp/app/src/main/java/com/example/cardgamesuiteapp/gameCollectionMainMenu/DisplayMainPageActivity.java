40204d3b6a760d14488c590bad43942a72124faf
Joel <joelkalich1111@gmail.com>
2021-01-19 19:52:19 -0800

    Fixed bug where pressing the back button would return to previous game

--- CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/gameCollectionMainMenu/DisplayMainPageActivity.java
+++ CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/gameCollectionMainMenu/DisplayMainPageActivity.java
@@ -13,11 +13,20 @@ import com.example.cardgamesuiteapp.games.Solitaire;
 
 public class DisplayMainPageActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_collection_activity_main_page);
         setContentView(R.layout.app_collection_content_main_page);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 
     @Override
