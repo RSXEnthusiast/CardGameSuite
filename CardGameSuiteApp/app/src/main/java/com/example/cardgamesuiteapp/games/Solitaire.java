77204b0c04ccd801850e3481cfee7d3fc39fd0ce
jgoodnight72 <jgoodnight@eagles.ewu.edu>
2021-01-29 17:35:46 -0800

    Adding java foundation to Solitaire

--- CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/games/Solitaire.java
+++ CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/games/Solitaire.java
@@ -14,7 +14,9 @@ import android.widget.TextView;
 public class Solitaire extends AppCompatActivity {
 
     static Standard deck;// The Deck object
+    static int player = 1; //only one player in solitaire
     static Card viewDiscard;// The discard view
+    static View viewDiscardHighlight;// Simply the "highlight" of th discard, mainly used for setting the highlight to visible/invisible
     static Card viewDeck;// The deck view
     static Button viewReturnToAppCollection;// The button the user will press to return to the main menu
     static Button viewReturnToGameMainMenu;// The button the user will press to return to the game's main menu
@@ -26,9 +28,29 @@ public class Solitaire extends AppCompatActivity {
         setContentView(R.layout.solitaire);
     }
 
+    private void initSolitaire() {
+        deck = new Standard(true, player);
+        viewDiscard = findViewById(R.id.discard);
+        viewDiscard.bringToFront();
+        viewDiscardHighlight = findViewById(R.id.highlightDiscard);
+        viewDiscardHighlight.setVisibility(View.INVISIBLE);
+        viewDeck = findViewById(R.id.deck);
+    }
+
     public void clickReturnToHome(View view) {
         Intent intent = new Intent(this, DisplayMainPageActivity.class);
         startActivity(intent);
+    }
+
+
+
+    //TODO METHODS
+    private static boolean hasWon() {
+        return false;
+    }
+
+    private static boolean hasLost() {
+        return false;
     }
 
 }
