2d43a1a65c2382991e0aaa39319d6c6c3bdf0d6e
jgoodnight72 <jgoodnight@eagles.ewu.edu>
2021-02-03 21:03:24 -0800

    Adding more java methods to solitaire that model after fives methods

--- CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/games/Solitaire.java
+++ CardGameSuiteApp/app/src/main/java/com/example/cardgamesuiteapp/games/Solitaire.java
@@ -20,12 +20,19 @@ public class Solitaire extends AppCompatActivity {
     static Card viewDeck;// The deck view
     static Button viewReturnToAppCollection;// The button the user will press to return to the main menu
     static Button viewReturnToGameMainMenu;// The button the user will press to return to the game's main menu
-    static TextView[] viewWinOrLose;// The textViews displays whether the user won or lost the game
+    static TextView viewWinOrLose;// The textViews displays whether the user won or lost the game
+    static solitaireStage stage;// The current stage of play
+    static boolean isAnimating;
+    static boolean preAnimation;
+    static int lastTouchedCardNum;
+    static boolean backButtonEnabled;
+    static int textColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.solitaire);
+        initSolitaire();
     }
 
     private void initSolitaire() {
@@ -35,6 +42,7 @@ public class Solitaire extends AppCompatActivity {
         viewDiscardHighlight = findViewById(R.id.highlightDiscard);
         viewDiscardHighlight.setVisibility(View.INVISIBLE);
         viewDeck = findViewById(R.id.deck);
+        viewWinOrLose = findViewById(R.id.winOrLose);
     }
 
     public void clickReturnToHome(View view) {
@@ -42,15 +50,119 @@ public class Solitaire extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * Walks through the stages of a players turn and switches the value of the enum and calls the relevant game method during each phase
+     *
+     * @param cardNum the value of the card that was tapped on
+     */
+    public static void cardTouched(int cardNum) {
+        if (!deck.isMyTurn() || isAnimating) {
+            return;
+        }
+        lastTouchedCardNum = cardNum;
+        switch (stage) {
+            case deal:
+                stageDeal();
+                break;
+            case cardSelected:
+                stageCardSelected(cardNum);
+                break;
+            case movingCard:
+                stageMovingCard(cardNum);
+                break;
+        }
+    }
 
+    /**
+     * This method is called when the deck is touched and the current stage is deal
+     */
+    private static void stageDeal() {
+        stage = solitaireStage.deal;
+        viewDeck.flipCard();
+        viewDiscardHighlight.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Called every time that there is a new game to reset the table
+     */
+    private void newGame() {
+        deck.shuffleDiscardIntoDeck();
+        try {
+            deck.deal(4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        viewDeck.setFaceUp(false);
+    }
+
+    /**
+     * This method updates every single dynamic item on the screen.
+     */
+    private static void updateEntireScreen() {
+        viewDiscard.updateImage(deck.peekTopDiscard());
+        viewDeck.updateImage(deck.peekTopDraw());
+        viewDeck.setFaceUp(false);
+    }
 
     //TODO METHODS
+    /**
+     * @return returns win or lose string depending on how the game ends
+     */
+    private static String getWinOrLose() {
+        String display = "";
+        if(stage.equals(solitaireStage.gameOver)) {
+            //only aces left, no cards in deck TODO if statement
+            display = "Game Over! You Win!";
+
+            //more than aces left, no cards in deck TODO if statement
+            display = "Game Over! You Lose";
+        }
+        return display;
+    }
+
+    /**
+     * A simple method to check whether there are any remaining moves or not
+     *
+     * @return true if the round is over, false if not.
+     */
+    private static boolean movesRemaining() {
+
+        //if no moves remaining TODO if statement
+        stage = stage.deal;
+        return true;
+    }
+
+    /**
+     * This method is called when a card is touched and the current stage is cardSelected
+     *
+     * @param cardNum the value of the card that was tapped on
+     */
+    private static void stageCardSelected(int cardNum) {
+
+    }
+
+    /**
+     * This method is called when a card is touched and the current stage is cardMoved
+     *
+     * @param cardNum the value of the card that was tapped on
+     */
+    private static void stageMovingCard(int cardNum) {
+
+    }
+
     private static boolean hasWon() {
         return false;
     }
 
     private static boolean hasLost() {
         return false;
+    }
+
+    /**
+     * The stages of the fives game turn
+     */
+    private enum solitaireStage {
+        deal, cardSelected, movingCard, gameOver
     }
 
 }
